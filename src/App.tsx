e7a8f8ad3c50d29eb198834c5e16ecb22c67c500
Mizki <minazukie2015@gmail.com>
2020-11-20 17:05:29 +0800

    update card style

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,15 @@ import { hot } from "react-hot-loader/root";
 const MainComponent = () => {
     const [form] = Form.useForm<{}>();
     return (
-        <Card style={{ margin: "20px auto", width: 600, borderRadius: 4 }}>
+        <Card
+            title="Demo"
+            style={{
+                margin: "20px auto",
+                width: 600,
+                borderRadius: 4,
+                boxShadow: "rgba(91, 100, 118, 0.21) 0px 0px 25px",
+            }}
+        >
             <DefaultComponent form={form} />
         </Card>
     );
